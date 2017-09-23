e4de44fa15737fd876fa366e12fd58636e64b9ad
Pamela Frieze <pfrieze@gmail.com>
2017-09-23 01:16:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
   #string with no additional formatting, 1-element array
   if array elements.length = 1
     array.join
-  #add and between elements in 2-element array
-  else array > 2-element
+  #add "and" between elements in 2-element array
+else array elements.lenght > 2
 array.insert()
-  #and 'and' before last element if array > three elements 
+  #and 'and' before last element if array > three elements
 end
 
 #array to string
