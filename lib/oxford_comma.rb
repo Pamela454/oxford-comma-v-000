6e3e03e39f45e84229cc9d54d5517bff8040a171
Pamela Frieze <pfrieze@gmail.com>
2017-09-23 01:10:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
 def oxford_comma(array)
+  #string with no additional formatting, 1-element array
   if array = [index]
     array.join 
-array.join(" and ")
+  #add and between elements in 2-element array
+  else array > 2-element
+array.insert()
+  #and 'and' before last element if array > three elements 
 end
 
 #array to string
