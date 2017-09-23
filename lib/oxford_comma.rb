480b917a1aae70a228492f9f553bea3355d15b67
Pamela Frieze <pfrieze@gmail.com>
2017-09-23 01:35:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ else array elements.length = 2
 array.insert( "and" )
   #and 'and' before last element if array > three elements
 else array elements.length > 3
-
+array.insert(-2, 'and') 
 end
 
 #array to string
