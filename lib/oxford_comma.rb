702b6797f06eb0bcc1242ccdd4df5c190afb7f41
Pamela Frieze <pfrieze@gmail.com>
2017-09-23 03:32:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ elsif array.length == 2
 array.insert( 'and' )
 #commas plus final and
 elsif array.length == 3
-  array.join(',')
+  
   #and 'and' before last element if array > three elements
 elsif array.length > 3
 array.insert(0, " and " )
