622aa4ccbe2ea6b920c809455eb9cc349933f6d6
Pamela Frieze <pfrieze@gmail.com>
2017-09-23 01:37:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
   #string with no additional formatting, 1-element array
-  if array elements.length = 1
+  if array.length = 1
     array.join
   #add "and" between elements in 2-element array
-else array elements.length = 2
+else array.length = 2
 array.insert( "and" )
   #and 'and' before last element if array > three elements
-else array elements.length > 3
+else array.length > 3
 array.insert(-2, 'and') 
 end
 
